e91c33e433ab414daf54d8e861f3fee7c8be3fb8
phillipkregg <phillipkregg@gmail.com>
2020-05-17 01:41:11 -0500

    Refactored some of the Post controller actions and tests to follow best practices

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,17 +14,19 @@ describe "navigate" do
   end
 
   describe "creation" do
-    it "has a new form that can be reached" do
+    before do
       visit new_post_path
+    end
+
+    it "has a new form that can be reached" do
       expect(page.status_code).to eq 200
     end
+
     it "has a page title of Create New Post" do
-      visit new_post_path
       expect(page).to have_content(/Create New Post/)
     end
 
     it "can be created from new form page" do
-      visit new_post_path
       fill_in "post[date]", with: Date.today
       fill_in "post[rationale]", with: "Some Rationale"
       click_on "Save"
