e21e0c4e6add2844de26d2a13dcdc8e5404187f6
phillipkregg <phillipkregg@gmail.com>
2020-05-17 01:06:27 -0500

    Used TDD to add create methods and show actions along with tests for Posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,23 @@ describe "navigate" do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe "creation" do
+    it "has a new form that can be reached" do
+      visit new_post_path
+      expect(page.status_code).to eq 200
+    end
+    it "has a page title of Create New Post" do
+      visit new_post_path
+      expect(page).to have_content(/Create New Post/)
+    end
+
+    it "can be created from new form page" do
+      visit new_post_path
+      fill_in "post[date]", with: Date.today
+      fill_in "post[rationale]", with: "Some Rationale"
+      click_on "Save"
+      expect(page).to have_content("Some Rationale")
+    end
+  end
 end
